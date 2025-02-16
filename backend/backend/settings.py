1a6e3ecf01ce020b3fced73098c836b8d6eb642a
Kumbbar <damirnasibullin8@gmail.com>
2025-02-16 20:36:45 +0500

    final compose

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -58,7 +60,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-import os
 
 DATABASES = {
     'default': {
